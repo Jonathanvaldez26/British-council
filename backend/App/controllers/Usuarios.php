ae0bfd209663abdd26f68c33105e2dcd7edd500a
OscarFernandoMillanPimentel <o_fernando_m_p@ciencias.unam.mx>
2022-02-18 13:37:06 -0600

    Validacion campos de datos (no permite numeros)

--- backend/App/controllers/Usuarios.php
+++ backend/App/controllers/Usuarios.php
@@ -536,9 +536,7 @@ html;
       $usuario->_contrasena = md5($contrasena);
 
       $usuarios = UsuarioDao::getAll();
-      //$usuario = $this->__usuario;
-      //$editarHidden = (Controller::getPermisosUsuario($usuario, "seccion_usuarios", 5)==1)? "" : "style=\"display:none;\"";
-      //$eliminarHidden = (Controller::getPermisosUsuario($usuario, "seccion_usuarios", 6)==1)? "" : "style=\"display:none;\"";
+
       $tabla= '';
       $status= '';
       foreach ($usuarios as $key => $value) {
@@ -569,18 +567,19 @@ html;
         }
         $tabla.=<<<html
                 <tr>
-                <td><h6 class="mb-0 text-sm">{$status}</h6></td>
-                <td><p class="text-sm text-secondary mb-0">{$value['nombre']}</p></td>
-                <td><p class="text-sm text-secondary mb-0">{$value['apellido_p']}</p></td>
-                <td><p class="text-sm text-secondary mb-0">{$value['apellido_m']}</p></td>
-                <td><p class="text-sm text-secondary mb-0">{$value['usuario']}</p></td>
-                <td><p class="text-sm text-secondary mb-0">{$tipo}</p></td>
-                
-                <td class="center" >
-                    <a href="/Usuarios/edit/{$value['administrador_id']}" type="submit" name="id" class="btn btn-outline-primary"><span class="fa fa-pencil-square-o"></span> </a>
-                    <!--<a href="/Usuarios/show/{$value['administrador_id']}" type="submit" name="id_empresa" class="btn btn-outline-success"><span class="fa fa-eye" ></span> </a>
-                    <button type="submit" name="id_empresa" class="btn btn-outline-info"><span class="fa fa-eye"></span></button>-->             
-                </td>
+                  <!-- td><input type="checkbox" name="borrar[]" value="{$value['administrador_id']}"/></td -->
+                  <td><h6 class="mb-0 text-sm">{$status}</h6></td>
+                  <td><p class="text-sm text-secondary mb-0">{$value['nombre']}</p></td>
+                  <td><p class="text-sm text-secondary mb-0">{$value['apellido_p']}</p></td>
+                  <td><p class="text-sm text-secondary mb-0">{$value['apellido_m']}</p></td>
+                  <td><p class="text-sm text-secondary mb-0">{$value['usuario']}</p></td>
+                  <td><p class="text-sm text-secondary mb-0">{$tipo}</p></td>
+                  
+                  <td class="center" >
+                      <a href="/Usuarios/edit/{$value['administrador_id']}" type="submit" name="id" class="btn btn-outline-primary"><span class="fa fa-pencil-square-o"></span> </a>
+                      <!--<a href="/Usuarios/show/{$value['administrador_id']}" type="submit" name="id_empresa" class="btn btn-outline-success"><span class="fa fa-eye" ></span> </a>
+                      <button type="submit" name="id_empresa" class="btn btn-outline-info"><span class="fa fa-eye"></span></button>-->             
+                  </td>
                 </tr>
 html;
      }
@@ -672,10 +671,8 @@ html;
 html;
         }
         $usuarios = UsuarioDao::getAll();
-      //$usuario = $this->__usuario;
-      //$editarHidden = (Controller::getPermisosUsuario($usuario, "seccion_usuarios", 5)==1)? "" : "style=\"display:none;\"";
-      //$eliminarHidden = (Controller::getPermisosUsuario($usuario, "seccion_usuarios", 6)==1)? "" : "style=\"display:none;\"";
-      $tabla= '';
+
+        $tabla= '';
       $status= '';
       foreach ($usuarios as $key => $value) {
           if($value['status'] = 1){
@@ -705,7 +702,6 @@ html;
         }
         $tabla.=<<<html
                 <tr>
-                <!--td><input type="checkbox" name="borrar[]" value="{$value['administrador_id']}"/></td-->
                 <td><h6 class="mb-0 text-sm">{$status}</h6></td>
                 <td><p class="text-sm text-secondary mb-0">{$value['nombre']}</p></td>
                 <td><p class="text-sm text-secondary mb-0">{$value['apellido_p']}</p></td>
