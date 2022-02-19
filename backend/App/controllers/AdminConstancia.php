0f138efebd90df9d08b309cd33361142392d056b
Gerson Velasco <gvelassco@gmail.com>
2022-02-19 13:43:03 -0600

    datatable contancias

--- backend/App/controllers/AdminConstancia.php
+++ backend/App/controllers/AdminConstancia.php
@@ -31,19 +31,19 @@ class AdminConstancia extends Controller{
 
     public function index() {
 
-      $header =<<<html
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-        <script src = "http://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js" defer ></script>
-        <link rel="stylesheet" href="http://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css" />
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.11.4/css/dataTables.bootstrap4.min.css" />
+      $extraFooter =<<<html
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+      <script src = "http://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js" defer></script>
+      <link rel="stylesheet" href="http://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css" />
+      <link rel="stylesheet" href="https://cdn.datatables.net/1.11.4/css/dataTables.bootstrap4.min.css" />
+      <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css" />
+      <script src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+      <script>
+        $(document).ready(function(){
+          $('#constanciasAll').DataTable();
           
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
-        <script src = "http://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js" defer ></script>
-          <!-- TEMPLATE VIEJO-->
-  </head>
+        });
+      </script>
 html;
       
       $usuarios = UsuarioDao::getAll();
